f738c2ce342c312fde6729b1dec4619bb3700a4d
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-07-12 23:00:43 -0300

    add better comment status display

--- client/src/components/comments/CommentsList.tsx
+++ client/src/components/comments/CommentsList.tsx
@@ -1,24 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+
 import CommentInterface from '../../interface/CommentInterface';
 
 export default function CommentList({ comments }: { comments: [] }) {
   const [commentList, setCommentList] = useState<CommentInterface[]>(comments);
 
-  const renderedComments = commentList.map((comment) => (
-    <li
-      key={comment.id}
-      className={`${
-        comment.status === 'approved'
-          ? 'bg-green-300'
-          : comment.status === 'rejected'
-          ? 'bg-red-300'
-          : 'bg-yellow-300'
-      }`}
-    >
-      {comment.content}
-    </li>
-  ));
+  const renderedComments = commentList.map((comment) => {
+    const commentStatus = comment.status;
+
+    let displayContent;
+    switch (commentStatus) {
+      case 'approved':
+        displayContent = comment.content;
+        break;
+      case 'rejected':
+        displayContent = 'comment rejected';
+        break;
+      case 'pending':
+        displayContent = 'comment pending moderation';
+        break;
+    }
+
+    return (
+      <li
+        key={comment.id}
+        className={`${
+          comment.status === 'approved'
+            ? 'bg-green-300'
+            : comment.status === 'rejected'
+            ? 'bg-red-300'
+            : 'bg-yellow-300'
+        }`}
+      >
+        {displayContent}
+      </li>
+    );
+  });
 
   return (
     <div>
